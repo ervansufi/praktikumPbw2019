e51548a8b9f5d415715a0a8cdf870bd81d267106
Nanda Syafitri <34921165+nandasyafitri@users.noreply.github.com>
2019-05-02 05:36:40 +0700

    Update profile.php

--- application/views/profile.php
+++ application/views/profile.php
@@ -4,17 +4,16 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">My Profile</div>
-                <?php if(isset($info)){
-                foreach($info as $data): ?>
+                
                 <div class="card-body">
-                    <form method="POST" action="<?php echo base_url('index.php/pesbuk/updateProfile')?>">
+                    <form method="POST" action="">
 
                         <div class="form-group row">
                             <label for="nama" class="col-md-4 col-form-label text-md-right">Name</label>
 
                             <div class="col-md-6">
-                                <input type="hidden" class="form-control" name="id" value="<?php echo $data['id']?>">
-                                <input type="text" class="form-control" name="name" value="<?php echo $data['name']?>">
+                                <input type="hidden" class="form-control" name="id" value="">
+                                <input type="text" class="form-control" name="name" value="">
                             </div>
                         </div>
 
@@ -22,7 +21,7 @@
                             <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
 
                             <div class="col-md-6">
-                                <input type="text" class="form-control" name="username" value="<?php echo $data['username']?>">
+                                <input type="text" class="form-control" name="username" value="">
                             </div>
                         </div>
 
@@ -30,7 +29,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
 
                             <div class="col-md-6">
-                                <input type="text" class="form-control" name="email" value="<?php echo $data['email']?>" >
+                                <input type="text" class="form-control" name="email" value="" >
 
                             </div>
                         </div>
@@ -51,8 +50,7 @@
                             </div>
                         </div>                       
                     </form>
-                    <?php endforeach?>
-                <?php }?>
+                }
                 </div>
             </div>
         </div>
