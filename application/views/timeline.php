b80ab67112b0999f2341d3d011ccfc546f39cf90
Nanda Syafitri <34921165+nandasyafitri@users.noreply.github.com>
2019-05-02 05:38:53 +0700

    Update timeline.php

--- application/views/timeline.php
+++ application/views/timeline.php
@@ -5,13 +5,13 @@
             <div class="card">
                 <div class="card-body text-center">
                     <img class="img img-responsive rounded-circle mb-3" width="160" src="<?php echo base_url('assets/img/default.svg')?>" />
-                    <p><?php echo $this->session->userdata('username'); ?></p>
+                    <p></p>
                 </div>
             </div>           
         </div>
 
         <div class="col-md-8">
-            <form action="<?php echo base_url('index.php/pesbuk/addStatus')?>" method="post" />
+            <form action="" method="post" />
                 <div class="form-group">
                     <textarea class="form-control" rows="5" name="status" id="status" placeholder="Apa yang kamu pikirkan?"></textarea>
                 </div>
@@ -26,13 +26,13 @@
 
       </div>
       <div class="col-md-8">
-      <?php foreach($info as $data): ?>
+      
             <div class="card mb-3">
                 <div class="card-body">
-                  <?php echo $data['status'];?>
+                  
                 </div>
             </div>
-      <?php endforeach  ?>
+
       </div>
     </div>
 </div>
